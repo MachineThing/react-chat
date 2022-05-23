022ab832870e39d26e4d3fe2cff90e888181028b
Mason Fisher <sackzombie10@gmail.com>
2022-05-23 09:08:30 -0400

    Message component

--- src/chat.js
+++ src/chat.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = props.nickname;
+    this.msg = props.msg;
+    this.style = {"color": "red"};
+
+  }
+
+  render() {
+    return(
+      <p><span style={this.style}>{this.name}</span>: {this.msg}</p>
+    )
+  }
+}
+
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      chats: []
+    };
+    this.state.chats.push({nickname:"Bot", msg:"Chats go here!"});
+
     this.props.handler.onmessage = function (event) {
       console.log(event.data);
     }
@@ -11,7 +33,11 @@ export default class Chat extends React.Component {
   render() {
     return(
       <div>
-        <p>Chats go here</p>
+        {this.state.chats.map(function(item) {
+          return(
+            <Message nickname={item.nickname} msg={item.msg} key={item}/>
+          )
+        })}
       </div>
     )
   }
